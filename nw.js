336105c97768282d5972f28bd870825c371ea62e
dszfsf <126802827+dszfsf@users.noreply.github.com>
2026-06-11 02:31:26 +0800

    Update nw.js

--- nw.js
+++ nw.js
@@ -4,20 +4,28 @@ const svc = new Service({
 	name: 'unblock-netease-cloud-music',
 	description: '点亮网易云音乐灰色歌曲',
 	script: './app.js', // 入口文件路径
-	// scriptOptions:'-p 52100:52101', // 可选参数示例: 自定义端口并开启HTTPS
-	// scriptOptions: '-o qq', // 可选参数
+	scriptOptions:'-p 5200:5201', // 可选参数示例: 自定义端口并开启HTTPS
+	scriptOptions: '-o qq kuwo kugou', // 可选参数
 	wait: '1', // 程序崩溃后重启时间间隔
 	grow: '0.25', // 重启等待时间成长值，第一次1秒，第二次1.25秒。。。
 	maxRestarts: '40', // 60秒内最大重启次数
 	env: [
-		// {
-		// 	name: 'ENABLE_FLAC',
-		// 	value 'true',
-		// },
-		// {
-		// 	name: 'QQ_COOKIE',
-		// 	value: 'uin=<your_uin>; qm_keyst=<your_qm_keyst>',
-		// },
+		   {
+	 	name: 'ENABLE_FLAC',
+         value 'true' },
+     {
+	name: 'QQ_COOKIE',
+	value: 'uin=<your_uin>; qm_keyst=<your_qm_keyst>',
+	},
+			   {
+	 	name: 'ENABLE_LOCAL_VIP',
+         value 'true' },
+			   {
+	 	name: 'BLOCK_ADS',
+         value 'true' },
+					   {
+	 	name: '	DISABLE_UPGRADE_CHECK',
+         value 'true' }
 	],
 });
 
