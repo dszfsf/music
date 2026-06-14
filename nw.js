9f48a381c2d3395ba313d0e566b83b2e18c92a35
dszfsf <126802827+dszfsf@users.noreply.github.com>
2026-06-15 04:16:40 +0800

    Update nw.js

--- nw.js
+++ nw.js
@@ -5,7 +5,6 @@ const svc = new Service({
 	description: '点亮网易云音乐灰色歌曲',
 	script: './app.js', // 入口文件路径
 	scriptOptions:'-p 5200:5201', // 可选参数示例: 自定义端口并开启HTTPS
-	scriptOptions: '-o qq kuwo kugou', // 可选参数
 	wait: '1', // 程序崩溃后重启时间间隔
 	grow: '0.25', // 重启等待时间成长值，第一次1秒，第二次1.25秒。。。
 	maxRestarts: '40', // 60秒内最大重启次数
